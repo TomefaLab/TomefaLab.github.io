82eafe2324d031a9660442ba6bcf57c7eee6aaac
TomefaLab <sergesvenceslas.com>
2024-06-27 18:02:38 -0700

    Mise à jour 18 de mon lab!

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-//Importer le chargemente ts on conteneur !
+document.body.style.overflow = "hidden";
+//Créer une variable contenant une instance du charment et son conteneur !
 const loaderContainer = document.querySelector(".loaderContainer");
 //Faire disparaitre le loader une fois la page complètement chargée !
 window.addEventListener("load",(e)=>{
+    //Faire fondre le chargement et son conteneur !
     loaderContainer.classList.add("fonduOut");
-    setTimeout((e) => {
+    //Faire disparaitre le chargment et son conteneur à la fin du fondu de ces dernuiers ( soit 400ms) !
+    setTimeout(() => {
         loaderContainer.classList.add("hidden");
     }, 400);
 });
