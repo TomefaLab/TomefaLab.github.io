04ece1df6d998bc03fbcd4b6f87cd84fd1f8e4fd
TomefaLab <sergesvenceslas.com>
2024-07-16 11:18:06 -0700

    mise a jour 40

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// document.body.style.overflow = "hidden";
-// //Créer une instance du loader et son conteneur !
-// const loaderContainer = document.querySelector(".loaderContainer");
-// //Faire disparaitre le loader une fois la page complètement chargée !
-// window.addEventListener("load",(e)=>{
-//     //Faire fondre le loader et son conteneur !
-//     loaderContainer.classList.add("fonduOut");
-//     //Faire disparaitre le loader et son conteneur juste a rpès le temps de fondu de la page !
-//     setTimeout((e) => {
-//         loaderContainer.classList.add("hidden");
-//         //Créactiver le scroll !
-//         document.body.style.overflow = "scroll";
-//     }, 400);
-// });
+//Désactiver le scroll de la page bien avant meme son chargement !
+document.body.style.overflow = "hidden";
+//Créer une instance du loader et son conteneur !
+const loaderContainer = document.querySelector(".loaderContainer");
+//Faire disparaitre le loader une fois la page complètement chargée !
+window.addEventListener("load",(e)=>{
+    //Faire fondre le chargement et son conteneur !
+    loaderContainer.classList.add("fonduOut");
+    //Masquer le loader et son conteneur juste après son fondu !
+    setTimeout(() => {
+        loaderContainer.classList.add("hidden");
+    }, 400);
+});
